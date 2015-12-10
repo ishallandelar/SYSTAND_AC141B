60fe49adc0bf3ae1c44409523c628079b1b992de
ishallandelar <llandelarc@gmail.com>
2015-12-10 11:26:18 +0800

    Create ac141-sale.sql

---
+++ ac141-sale.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 10, 2015 at 04:09 AM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ac141_sale`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale`
+--
+
+CREATE TABLE IF NOT EXISTS `sale` (
+  `id` int(11) NOT NULL,
+  `patients_name` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `birth_date` varchar(20) NOT NULL,
+  `ward/service` varchar(100) NOT NULL,
+  `tel._no` int(11) NOT NULL,
+  `age` int(2) NOT NULL,
+  `nationality` varchar(20) NOT NULL,
+  `date_admitted` int(11) NOT NULL,
+  `date_discharge` int(11) NOT NULL,
+  `sex` varchar(1) NOT NULL,
+  `civil_status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sale`
+--
+ALTER TABLE `sale`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sale`
+--
+ALTER TABLE `sale`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
